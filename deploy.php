719e6af5bd07f15ba2f7d887adacd7e404340771
Aaron <aaron.n.kleine@proton.me>
2024-07-15 14:27:41 +0200

    Initial deployment

---
+++ deploy.php
@@ -0,0 +1,30 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'git@github.com:aaronnkleine/TodoApp_Laravel.git');
+
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('staging')
+    ->setHostname('37.202.2.195')
+    ->set('remote_user', 'p673504f1')
+    ->set('deploy_path', '~/TodoApp_Laravel/staging');
+
+
+host('production')
+    ->setHostname('37.202.2.195')
+    ->set('remote_user', 'p673504f1')
+    ->set('deploy_path', '~/TodoApp_Laravel/production');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
